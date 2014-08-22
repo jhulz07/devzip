d94791867e003cc9bee1f00b54ddc19d95b86bf7
MJD <markjd.is.it@gmail.com>
2014-08-22 13:57:24 +0800

    Removed Comments

--- application/views/home.php
+++ application/views/home.php
@@ -6,16 +6,13 @@
 <body>    
     <?php
         echo "<h3>". str_replace('\\', ' > ', str_replace('c:\xampp\htdocs', 'web_root', $this->session->userdata('current_path'))) . "</h3>";
-       // print_r($selected_dir);
-
-        //TODO: if the item is a folder then load another folder, if it's a file, allow the user to select it but display options for the file
         if ($this->session->userdata('path_is_root')==FALSE) {
         ?><a href="<?php echo base_url(); ?>">.\ web_root</a><br><a href="<?php echo base_url(); ?>ctrl/up">.. up one level</a><br><br><?php
         } 
         $dir_x=-1;
         foreach ($selected_dir as $dir => $dir_node) {
             $dir_x++;
-            if(gettype($dir_node)=='array') { // TODO: add detection of folder here, we may don't like to display files.
+            if(gettype($dir_node)=='array') {
             ?><a href="<?php echo base_url() . "ctrl/enter/" . $dir_x; ?>"><?php echo $dir; ?></a><br><?php   
             } else {
                 ?><a href="<?php echo base_url() . "ctrl/enter/" . $dir_x; ?>"><?php echo $dir_node; ?></a><br><?php   
